b0a4e27fd5e3c0a3765e858dceff9ad0991a9260
hjdr <harry.jdr@gmail.com>
2019-09-01 21:50:16 +0100

    added todos & todoItem components

---
+++ src/components/TodoItem.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class TodoItem extends React.Component {
+
+  render() {
+
+    return (
+      <div>
+        <p> {this.props.todo.title } </p>
+      </div>
+    )
+  }
+} 
+
+export default TodoItem
